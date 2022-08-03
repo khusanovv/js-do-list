8b8d95f5a9d095e4999af60162ad63ee70fe93a4
khusanovv <boburshohkhusanov@gmail.com>
2022-08-03 11:44:38 +0500

    update app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+const title = document.querySelector('#title'),
+    author = document.querySelector('#author',),
+    year = document.querySelector('#year'),
+    btn = document.querySelector('.btn'),
+    bookList = document.querySelector('#book-list');
+
+btn.addEventListener('click',(event) =>{
+    event.preventDefault()
+
+
+    if(title.value == '' && author.value == '' && year.value ==''){
+        alert('Toldiring')
+
+    }
+    else {
+        const newRow = document.createElement('tr')
+
+        const newTitle = document.createElement('th')
+        newTitle.innerHTML = title.value
+        newRow.appendChild(newTitle);
+
+        const newAuter = document.createElement('th')
+        newAuter.innerHTML = title.value
+        newRow.appendChild(newAuter);
+
+        const newYear = document.createElement('th')
+        newYear.innerHTML = title.value
+        newRow.appendChild(newYear);
+
+        bookList.appendChild(newRow)
+
+        title.value = ''
+        author.value = ''
+        year.value = ''
+
+
+
+
+
+    }
+} )
+
+
